e2cc7b6c269cc32ad867a249ade117b26719b304
kaspnarov <81401532+kaspnarov@users.noreply.github.com>
2021-03-28 05:33:58 -0300

    Update Lab1.c

--- Lab1.c
+++ Lab1.c
@@ -1,7 +1,6 @@
 /* Disciplina: Computacao Concorrente */
 /* Prof.: Silvana Rossetto */
 /* Módulo 1 - Laboratório: 1 */
-/* Codigo: "Hello World" usando threads em C */
 
 #include <stdio.h>
 #include <stdlib.h> 
@@ -10,53 +9,46 @@
 #define NTHREADS 2
 int Vetor[10000];
 
-//funcao de uma thread que incrementa em 1 as ultimas 5000 casas do Vetor
-void *ContaSup (void *arg){
-    for(int i=5000; i<10000; i++){
+//funcao que incrementa em 1 metade das casas do Vetor
+void *AddUm (void *arg){
+    int base = * (int *) arg;
+    for(int i=base; i<base+5000; i++){
         Vetor[i] += 1;
     }
     pthread_exit(NULL);
 }
 
-//funcao de uma thread que incrementa em 1 as primeiras 5000 casas do Vetor
-void *ContaInf (void *arg){
-    for(int i=0; i<5000; i++){
-        Vetor[i] += 1;
-    }
-    pthread_exit(NULL);
-}
-
-//--funcao principal do programa
+//funcao principal do programa
 int main(void) {
     pthread_t tid[NTHREADS]; //identificadores das threads no sistema
-    int TudoTop = 1; //variavel para confirmacao
+    int TudoTop = 1; //variavel para confirmacao final
+    int baseInf = 0; //base para o parmetro da primeira thread
+    int baseSup = 5000; //base para o parametro da segunda thread
     for(int i=0; i<10000; i++){ //inicializacao do Vetor
         Vetor[i] = 0;
     }
 
-    if (pthread_create(&tid[0], NULL, ContaInf, NULL)) {
+    if (pthread_create(&tid[0], NULL, AddUm, (void *)&baseInf)) { //cria a primeira thread
         printf("--ERRO: pthread_create() ContaInf\n"); exit(-1);
     }
-    if (pthread_create(&tid[1], NULL, ContaSup, NULL)) {
+    if (pthread_create(&tid[1], NULL, AddUm, (void *)&baseSup)) { //cria a segunda thread
         printf("--ERRO: pthread_create() ContaSup\n"); exit(-1);
     }
 
-    for(int i=0; i<NTHREADS; i++){
+    for(int i=0; i<NTHREADS; i++){ //espera as threads terminarem o processo
         if(pthread_join(tid[i], NULL))
             printf("--ERRO: pthread_join()\n");
     }
     for(int i=0; i<10000; i++){ //checa se o Vetor foi incrementado em 1
         if(Vetor[i] != 1){
-            printf("%d", Vetor[i]);
             TudoTop = 0;
         }
     }
     if(TudoTop) //se o Vetor tiver sido incrementado, imprime uma mensagem positiva
-        printf("Tudo top rs\n");
-    else //senão, imprime uma mensagem negatia
+        printf("O Vetor foi incrementado corretamente\n");
+    else //senão, imprime uma mensagem negativa
         printf("Deu ruim :(\n");
         
-    printf("--Thread principal terminou\n");
 
     pthread_exit(NULL);
 }
